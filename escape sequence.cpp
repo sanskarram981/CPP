7d662ab253c2cc3fcf7680776fe4078219dc064f
Sanskar Ram <71223200+sanskarram981@users.noreply.github.com>
2023-05-31 08:48:38 +0530

    Create escape sequence.cpp

---
+++ escape sequence.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    std::cout<<"the \nshape \nof \nthe \nearth\n";
+    std::cout<<"the \tshape \tof \tthe \tearth\n";
+    std::cout<<"the \bshape \bof \bthe \bearth\n";
+    std::cout<<"the \"shape\" of the earth\n";
+    std::cout<<"the \\shape\\ of the earth";
+
+
+    return 0;
+}
